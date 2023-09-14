92d3475ab7aa55262e51dd2b2288c16ad00933e1
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2023-09-14 20:45:48 +0200

    init commit

---
+++ src/main/java/com/gokul/lifestyleapp/LifestyleappApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.lifestyleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifestyleappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifestyleappApplication.class, args);
+	}
+
+}
